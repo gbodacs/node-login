a8be33b30d8c1fe2956286e98de456f17d4af6ce
Andor Auber <andor.auber@gmail.com>
2019-10-07 19:49:49 +0200

    refactor(footer): simplify footer structure

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -4,41 +4,47 @@ import Card from 'react-bootstrap/Card';
 
 class Footer extends React.Component {
   render() {
+    const footerbody = (
+      <div>
+        <Card.Header>
+          <small>BRIGITTA GYÓGYTORNÁSZ</small>
+        </Card.Header >
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
 
-    return (<Card bg="light">
-      <Card.Header>
-        <small>BRIGITTA GYÓGYTORNÁSZ</small>
-      </Card.Header >
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
+            <Card.Body className={'d-flex justify-content-sm-around'}>
+              <div>
+                <small>CÍM: 1000 Budapest, Kossuth Lajos utca 1.</small>
+                <br></br>
+                <small>TELEFON: +36-00-000-0000</small>
+                <br></br>
+                <small>E-MAIL: brigitta@gmai.com</small>
+              </div>
+              <br></br>
+              <div>
+                <Card.Text>
+                  <small>Itt is követhetsz:</small>
+                </Card.Text>
+                <i className="fab fa-facebook-square"></i>
+                <Card.Link href="#"><small> FACEBOOK </small></Card.Link>
+                <i className="fab fa-youtube"></i>
+                <Card.Link href="#"><small> YOUTUBE </small></Card.Link>
+                <i className="fab fa-blogger"></i>
+                <Card.Link href="#"><small> BLOG </small></Card.Link>
+              </div>
+            </Card.Body>
+          </blockquote>
+        </Card.Body>
+      </div>
+    );
 
-          <Card.Body className={'d-flex justify-content-sm-around'}>
-            <div>
-              <small>CÍM: 1000 Budapest, Kossuth Lajos utca 1.</small>
-              <br></br>
-              <small>TELEFON: +36-00-000-0000</small>
-              <br></br>
-              <small>E-MAIL: brigitta@gmai.com</small>
-            </div>
-            <br></br>
-            <div>
-              <Card.Text>
-                <small>Itt is követhetsz:</small>
-              </Card.Text>
-              <i className="fab fa-facebook-square"></i>
-              <Card.Link href="#"><small> FACEBOOK </small></Card.Link>
-              <i className="fab fa-youtube"></i>
-              <Card.Link href="#"><small> YOUTUBE </small></Card.Link>
-              <i className="fab fa-blogger"></i>
-              <Card.Link href="#"><small> BLOG </small></Card.Link>
-            </div>
-          </Card.Body>
-        </blockquote>
-      </Card.Body>
-      <Card.Footer className="blockquote-footer">
-        © 2019 Minden jog fenntartva | Az oldalt a TEAM működteti
-      </Card.Footer>
-    </Card>);
+    return (
+      <Card bg="light" className="footer fixed-bottom font-italic">
+        <Card.Footer>
+          © 2019 Minden jog fenntartva | Az oldalt a TEAM működteti
+        </Card.Footer>
+      </Card>
+    );
   }
 }
 
