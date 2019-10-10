522ea48d1066cdc4cfcf35adb3c8a7a00eb22a2c
Szilvia Kis <kisszilvia7@gmail.com>
2019-10-10 23:53:54 +0200

    feat(style): simplify the footer and add second version

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -4,48 +4,66 @@ import Card from 'react-bootstrap/Card';
 
 class Footer extends React.Component {
   render() {
-    const footerbody = (
-      <div>
-        <Card.Header>
-          <small>BRIGITTA GYÓGYTORNÁSZ</small>
-        </Card.Header >
-        <Card.Body>
-          <blockquote className="blockquote mb-0">
-
-            <Card.Body className={'d-flex justify-content-sm-around'}>
-              <div>
-                <small>CÍM: 1000 Budapest, Kossuth Lajos utca 1.</small>
-                <br></br>
-                <small>TELEFON: +36-00-000-0000</small>
-                <br></br>
-                <small>E-MAIL: brigitta@gmai.com</small>
-              </div>
-              <br></br>
-              <div>
-                <Card.Text>
-                  <small>Itt is követhetsz:</small>
-                </Card.Text>
-                <i className="fab fa-facebook-square"></i>
-                <Card.Link href="#"><small> FACEBOOK </small></Card.Link>
-                <i className="fab fa-youtube"></i>
-                <Card.Link href="#"><small> YOUTUBE </small></Card.Link>
-                <i className="fab fa-blogger"></i>
-                <Card.Link href="#"><small> BLOG </small></Card.Link>
-              </div>
-            </Card.Body>
-          </blockquote>
-        </Card.Body>
-      </div>
-    );
-
-    return (
-      <Card bg="white" className="footer fixed-bottom font-italic">
-        <Card.Footer>
-          © 2019 Minden jog fenntartva | Az oldalt a TEAM működteti
-        </Card.Footer>
-      </Card>
-    );
+    return (<Card bg="white" className="footer fixed-bottom font-bold">
+      <Card.Footer>
+        <Card.Link>Copyright © 2019 | Az oldalt a TEAM működteti</Card.Link>
+        <Card.Link>Kapcsolat</Card.Link>
+        <Card.Link>Adatvédelmi elvek</Card.Link>
+        <Card.Link>Süti szabályzat</Card.Link>
+      </Card.Footer>
+    </Card>);
   }
 }
 
 export default Footer;
+
+/*
+import React from "react";
+import './footer.scss';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+
+class Footer extends React.Component {
+  render() {
+    return (
+    <MDBFooter bg="white" className="footer font-bold pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Kapcsolat</h5>
+            <p>
+              Ide írhatnánk majd Brigi elérhetőségeit
+            </p>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">Adatvédelmi elvek</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Süti szabályzat</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">FACEBOOK</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">YOUTUBE</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">BLOG</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid className ="fixed-bottom">
+          &copy; {new Date().getFullYear()} Copyright
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+}
+}
+
+export default Footer;*/
