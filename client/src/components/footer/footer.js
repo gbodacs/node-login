42d6116c325df0f59911504505ab211227d5f167
Gabor Bodacs <gbodacs@gmail.com>
2019-10-17 10:23:31 +0200

    feat(terms_cookies): Added tabs and links to the footer

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -7,9 +7,9 @@ class Footer extends React.Component {
     return (<Card bg="white" className="footer fixed-bottom font-bold text-center">
       <Card.Footer>
         <Card.Link>Copyright © 2019 | Az oldalt a TEAM működteti</Card.Link>
-        <Card.Link>Kapcsolat</Card.Link>
+        <Card.Link href="/connection">Kapcsolat</Card.Link>
         <Card.Link href="/terms">Adatvédelmi elvek</Card.Link>
-        <Card.Link>Süti szabályzat</Card.Link>
+        <Card.Link href="/terms">Süti szabályzat</Card.Link>
       </Card.Footer>
     </Card>);
   }
--- client/src/pages/terms_cookies/terms_cookies.js
+++ client/src/pages/terms_cookies/terms_cookies.js
@@ -2,20 +2,76 @@ import React from 'react';
 import './terms_cookies.scss';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-// import CardLink from 'react-bootstrap/Card';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 class Terms extends React.Component {
   constructor(props) {
     super(props);
     this.redirectToHomePage = this.redirectToHomePage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      text: "default2",
+      value: "data"
+    };
   }
 
   redirectToHomePage() {
     this.props.history.push('/home');
   }
+  
+   //const [value, setValue] = React.useState(0);
+
+  handleChange(event, newValue)
+  {
+    if (newValue === "data")
+    {
+      this.setState({
+        text: "data text",
+        value: "data"
+      });
+    }
+    
+    if (newValue === "cookie")
+    {
+      this.setState({
+        text: "kuki text",
+        value: "cookie"
+      });
+    }
+
+    console.log(event);
+    console.log(newValue);
+  }
 
   render() {
-    return (
+    return (  
+      <Paper className="Terms mx-auto my-5">
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab label="Adatvédelmi elvek" value = "data"/>
+          <Tab label="Süti szabályzat" value = "cookie"/>
+        </Tabs>
+        <Card className="Terms p-2 mx-auto my-5">
+        <Card.Body>
+          <Card.Title>Adatvédelmi és Adatkezelési szabályzat</Card.Title>
+          <Card.Text>
+            {this.state.text}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <Button variant="primary" type="button" onClick={this.redirectToHomePage} className={`mt-4`}>Vissza a főoldalra</Button>
+        </Card.Footer>
+      </Card>
+      </Paper>
+    );
+    /*return (
       <Card className="Terms p-2 mx-auto my-5">
         <Card.Body>
           <Card.Title>Adatvédelmi és Adatkezelési szabályzat</Card.Title>
@@ -27,7 +83,7 @@ class Terms extends React.Component {
           <Button variant="primary" type="button" onClick={this.redirectToHomePage} className={`mt-4`}>Vissza a főoldalra</Button>
         </Card.Footer>
       </Card>
-    );
+    );*/
   }
 }
 
