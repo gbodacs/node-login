a7095326db21b6e6a082dfd0c881e8251aa69cdb
Gabor Bodacs <gbodacs@gmail.com>
2019-10-17 15:18:50 +0200

    feat(terms_cookies): replace missing data with ********

--- client/src/pages/terms_cookies/terms_cookies.js
+++ client/src/pages/terms_cookies/terms_cookies.js
@@ -9,35 +9,34 @@ import Footer from '../../components/footer/footer';
 
 class Terms extends React.Component {
   dataTitle = `Adatvédelmi és Adatkezelési szabályzat`;
-  dataText = `  Adatkezelő: BRIGI (székhely: BRIGI címe), amely önállóan az adatok kezelésének célját meghatározza, az adatkezelésre vonatkozó döntéseket meghozza, végrehajtja, vagy az általa megbízott adatfeldolgozóval végrehajtatja.
+  dataText = `  Adatkezelő: BRIGI************ (székhely: BRIGI************ címe), amely önállóan az adatok kezelésének célját meghatározza, az adatkezelésre vonatkozó döntéseket meghozza, végrehajtja, vagy az általa megbízott adatfeldolgozóval végrehajtatja.
   Adatkezelés: az alkalmazott eljárástól függetlenül az adatokon végzett bármely művelet vagy a műveletek összessége, így különösen gyűjtése, felvétele, rögzítése, rendszerezése, tárolása, megváltoztatása, felhasználása, lekérdezése, továbbítása, nyilvánosságra hozatala, összehangolása vagy összekapcsolása, zárolása, törlése és megsemmisítése, valamint az adatok további felhasználásának megakadályozása.
   Adatfeldolgozó: BRIGI (székhely: BRIGI címe).
   Adatfeldolgozás: az adatkezelési műveletekhez kapcsolódó technikai feladatok elvégzése, függetlenül a műveletek végrehajtásához alkalmazott módszertől és eszköztől, valamint az alkalmazás helyétől, feltéve hogy a technikai feladatot az adatokon végzik;
   
   1. Milyen adatokat kezelünk?
-  Az Adatkezelő a saját weboldalán (http://brigi.hu) regisztrációt nem biztosít, csak meghívás alapján csatlakozhatnak az oldalhoz. Ezt követően a kapott jelszóval beléphet a Felhasználó.
-  BRIGI Kft. adatkezelési nyilvántartási száma: AAAA-00000/2019.  ???
+  Az Adatkezelő a saját weboldalán (http://brigi.hu)************ regisztrációt nem biztosít, csak meghívás alapján csatlakozhatnak az oldalhoz. Ezt követően a kapott jelszóval beléphet a Felhasználó.
+  BRIGI Kft. adatkezelési nyilvántartási száma: AAAA-00000/2019.  ************
   A jelentkezés során az Adatkezelő a felhasználó alábbi adatait kezelheti:
     a) Felhasználó neve;
     b) Felhasználó e-mail címe;
-    c) Felhasználó országa ?????
-  A fenti adatokat az Adatkezelő elektronikus formában tárol el. Az adatokat csak a terápia céljából használja, hogy személyes...
+    c) Felhasználó országa;
+  A fenti adatokat az Adatkezelő elektronikus formában tárol el. Az adatokat csak a terápia céljából használja, hogy személyre szabott szolgáltatást biztosítson.
   
   2. Mi az adatkezelés célja, jogalapja?
-    Az adatkezelés jogalapja: 
-    Az adatkezelés célja: 
-    Kik ismerhetik meg pályázatát?
+    Az adatkezelés jogalapja: ************
+    Az adatkezelés célja: ************
     A Felhasználó személyes adatait az Adatkezelő bizalmasan kezeli, a Felhasználó tudta és hozzájárulása nélkül információt nem továbbít illetéktelen harmadik személy részére.
   
-  3. Hogyan és mennyi ideig kezeljük pályázati anyagát?
+  3. Hogyan és mennyi ideig kezeljük adatait?
     Jelen tájékoztató elfogadásával a Felhasználó kifejezetten hozzájárul ahhoz, hogy az általa megadott adatokat az Adatkezelő a Felhasználó napjától a Felhasználó törlési kérelméig kezelje (különösen gyűjtse, rögzítse, rendszerezze, tárolja, felhasználja, lekérdezze, törölje stb).
-  *** A Felhasználó bármikor kérheti a személyes adatai törlését a brigi@brigie.hu e-mail címen.
+  *** A Felhasználó bármikor kérheti a személyes adatai törlését a ************ e-mail címen.
   
   4. Kihez fordulhat személyes adatai kezelésével kapcsolatosan?
   A Felhasználó az Adatkezelő által kezelt személyes adatairól tájékoztatást, azok helyesbítését, törlését vagy zárolását kérheti, továbbá tiltakozhat a személyes adatainak kezelése ellen az alábbi elérhetőségek bármelyikén.
-  Név: 
-  Cím: 
-  E-mail: 
+  Név: ************
+  Cím: ************
+  E-mail: ************
   A Felhasználó kérelmére az Adatkezelő tájékoztatást ad az általa kezelt, illetve az Adatfeldolgozó által feldolgozott adatairól, az adatkezelés céljáról, jogalapjáról, időtartamáról, az Adatfeldolgozó adatkezeléssel összefüggő tevékenységéről. Az Adatkezelő köteles a kérelem benyújtásától számított legrövidebb idő alatt, legfeljebb azonban 30 napon belül közérthető formában, az érintett erre irányuló kérelmére írásban megadni a tájékoztatást.
   
   5. Milyen jogorvoslati lehetőséggel élhet a személyes adatai kezelésével kapcsolatosban?
